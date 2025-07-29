5b93152122501100b7dd1a31e3217783aae679f6
Derrick Asamoah <asamoahderrick7@gmail.com>
2025-07-29 17:20:02 +0000

    fix

---
+++ server/api/tasks/[id]/index.put.ts
@@ -0,0 +1,55 @@
+import { executeUpdateTask } from "~/lib/clientQueries";
+
+export default defineEventHandler(async (event) => {
+  try {
+    const id = getRouterParam(event, "id");
+    const body = await readBody(event);
+    
+    const updateExpressions: string[] = [];
+    const expressionAttributeValues: Record<string, any> = {};
+    const expressionAttributeNames: Record<string, string> = {};
+    let updateExpressionString = "SET ";
+
+    if (body.title !== undefined) {
+      updateExpressions.push("#t = :title");
+      expressionAttributeValues[":title"] = body.title;
+      expressionAttributeNames["#t"] = "title";
+    }
+    if (body.dueDate !== undefined) {
+      updateExpressions.push("#dd = :dueDate");
+      expressionAttributeValues[":dueDate"] = body.dueDate;
+      expressionAttributeNames["#dd"] = "dueDate";
+    }
+    if (body.isCompleted !== undefined) {
+      updateExpressions.push("#ic = :isCompleted");
+      expressionAttributeValues[":isCompleted"] = body.isCompleted;
+      expressionAttributeNames["#ic"] = "isCompleted";
+    }
+
+    if (updateExpressions.length === 0) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: "No fields to update",
+      });
+    }
+
+    updateExpressionString += updateExpressions.join(", ");
+
+    const response = await executeUpdateTask({
+      Key: { id },
+      UpdateExpression: updateExpressionString,
+      ExpressionAttributeValues: expressionAttributeValues,
+      ExpressionAttributeNames: expressionAttributeNames,
+    });
+    return response?.Attributes;
+  } catch (error) {
+    console.error(
+      `Error updating note with ID ${getRouterParam(event, "id")}:`,
+      error
+    );
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Failed to update note",
+    });
+  }
+});
---
+++ server/api/tasks/index.post.ts
@@ -0,0 +1,26 @@
+import { v4 as uuidv4 } from "uuid";
+import { executeCreateTask } from "~/lib/clientQueries";
+
+
+export default defineEventHandler(async (event) => {
+  try {
+    const body = await readBody(event);
+    const newTask = {
+      id: uuidv4(),
+      title: body.title,
+      createdAt: new Date().getTime(),
+      dueDate: body.dueDate || null,
+      isCompleted: body.isCompleted || false,
+    };
+
+		await executeCreateTask(newTask);
+
+    return newTask;
+  } catch (error) {
+    console.error("Error creating note:", error);
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Failed to create note",
+    });
+  }
+});
